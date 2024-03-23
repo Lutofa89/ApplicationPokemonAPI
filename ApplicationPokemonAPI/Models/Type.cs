7193f96fbb235f8b32beea70db725a66094b5b44
Fabrice K. <kerremans.1904.student@ifosupwavre.be>
2024-03-23 22:33:44 +0100

    J'ai ajouté dans le dossier Model plusieurs fichier, plusieurs class.
    Classe :
                Srites
                Stat
                Stat2
                Type
                Type2

---
+++ ApplicationPokemonAPI/Models/Type.cs
@@ -0,0 +1,10 @@
+﻿namespace ApplicationPokemonAPI.Models
+{
+    public class Type
+    {
+        public int slot { get; set; }
+        public Type2 type { get; set; }
+    }
+
+    
+}
---
+++ ApplicationPokemonAPI/Models/Type2.cs
@@ -0,0 +1,9 @@
+﻿namespace ApplicationPokemonAPI.Models
+{
+
+    public class Type2
+    {
+        public string name { get; set; }
+        public string url { get; set; }
+    }
+}
