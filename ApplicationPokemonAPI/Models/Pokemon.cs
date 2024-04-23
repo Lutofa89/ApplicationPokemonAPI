92227ea05e4dadbd9986eb426010f09a5e586c65
Fabrice K. <kerremans.1904.student@ifosupwavre.be>
2024-04-23 21:59:23 +0200

    J'ai un petit problème :   var favoriteJson = await JSRuntime.InvokeAsync<string>("localStorage.getItem", "favorites"); On verra pour le résoudre plus tard.

--- ApplicationPokemonAPI/Models/Pokemon.cs
+++ ApplicationPokemonAPI/Models/Pokemon.cs
@@ -11,6 +11,8 @@
         public Sprites sprites { get; set; }
         public List<Stat> stats { get; set; }
         public List<Type> types { get; set; }
+
+        public bool IsFavorite { get; set; } =false; // ajoutez une propriété IsFavorite si vous souhaitez suivre l'état de favori du Pokémon
     }
 
 }
